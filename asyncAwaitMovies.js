09a520d1e51b4eff929e6549cd9657ec03cf8517
Shashank Sabniveesu <shashank@linux.com>
2021-08-06 20:34:57 -0400

    Added "File Write" operation and "multiple exception handling" - NOTE: To trigger the 'TypeError' you just have to remove the 'await' keyword against the 'axios.get(...)' call

--- asyncAwaitMovies.js
+++ asyncAwaitMovies.js
@@ -2,17 +2,26 @@ const axios = require('axios');
 const fs = require('fs').promises;
 
 async function saveMovies(){
+	let movieList = '';
+	
 	try{
 		let response = await axios.get('https://ghibliapi.herokuapp.com/films');
 
-		let movieList = '';
 		response.data.forEach(movie => {
 			movieList += `${movie['title']}, ${movie['release_date']}\n`;
 		});
+
+		await fs.writeFile('asyncAwaitMovies.csv', movieList);
 	}
 	catch(error){
-		console.log(`Caught error: ${error}`);
+		if(error instanceof TypeError){
+			console.log('That is a TypeError');
+		}
+		else{
+			console.log(`caught error in any of the many operations: ${error}. Error type is not easy to find out in NodeJS. You should have thrown custom exceptions yourself where they are expected`)
+		}
 	}
+
 }
 
 saveMovies();
