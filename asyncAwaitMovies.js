416ed56ad4848815abedc357cd4ebb68469768d4
Shashank Sabniveesu <shashank@linux.com>
2021-08-06 20:01:14 -0400

    Defined an "async" function that "await"s a function returning a promise inside a "try-catch block"

--- asyncAwaitMovies.js
+++ asyncAwaitMovies.js
@@ -1,1 +1,18 @@
 const axios = require('axios');
+const fs = require('fs').promises;
+
+async function saveMovies(){
+	try{
+		let response = await axios.get('https://ghibliapi.herokuapp.com/films');
+
+		let movieList = '';
+		response.data.forEach(movie => {
+			movieList += `${movie['title']}, ${movie['release_date']}\n`;
+		});
+	}
+	catch(error){
+		console.log(`Caught error: ${error}`);
+	}
+}
+
+saveMovies();
